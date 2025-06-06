75ba368c302258819f0a08b49d9671ff9c89f3b8
Tymoteusz Tomczak <tymoteusztom@gmail.com>
2025-06-06 18:04:50 +0200

    poprawka nazw zmiennych dla wygodniejszej pracy

--- s_01_podzial_danych.m
+++ s_01_podzial_danych.m
@@ -4,32 +4,37 @@ load('RWP.dat')
 czesc_danych_do_identyfikacji = 0.7;
 ident_pierwsza = true;
 
-u = RWP(:,1);
-y = RWP(:,2);
+u_full = RWP(:,1);
+y_full = RWP(:,2);
 % w = RWP(:,3);
-N = length(u);
+N = length(u_full);
 
 % 100Hz sampling frequency
-t = 0:0.01:(length(u)-1)*0.01;
-t = t';
+t_full = 0:0.01:(length(u_full)-1)*0.01;
+t_full = t_full';
 
 N_cut = floor(N * czesc_danych_do_identyfikacji);
 
 if ident_pierwsza
-    u_indentyfikacyjne = u(1:N_cut);
-    u_weryfikacyjne = u((N_cut+1):end);
-    y_indentyfikacyjne = y(1:N_cut);
-    y_weryfikacyjne = y((N_cut+1):end);
-    t_indentyfikacyjne = t(1:N_cut);
-    t_weryfikacyjne = t(1:(N-N_cut));
+    u = u_full(1:N_cut);
+    y = y_full(1:N_cut);
+    t = t_full(1:N_cut);
+    save("dane_identyfikacyjne","u","y","t");
+
+    u = u_full((N_cut+1):end);
+    y = y_full((N_cut+1):end);
+    t = t_full(1:(N-N_cut));
+    save("dane_weryfikacyjne","u","y","t");
 else
-    u_weryfikacyjne = u(1:(N - N_cut));
-    u_indentyfikacyjne = u((N - N_cut+1):end);
-    y_weryfikacyjne = y(1:(N - N_cut));
-    y_indentyfikacyjne = y((N - N_cut+1):end);
-    t_weryfikacyjne = t(1:(N - N_cut));
-    t_indentyfikacyjne = t(1:N_cut);
+    u = u_full(1:(N - N_cut));
+    y = y_full(1:(N - N_cut));
+    t = t_full(1:(N - N_cut));
+    save("dane_weryfikacyjne","u","y","t");
+
+    u = u_full((N - N_cut+1):end);
+    y = y_full((N - N_cut+1):end);
+    t = t_full(1:N_cut);
+    save("dane_identyfikacyjne","u","y","t");
 end
 
-save("dane_identyfikacyjne","u_indentyfikacyjne","y_indentyfikacyjne","t_indentyfikacyjne");
-save("dane_weryfikacyjne","u_weryfikacyjne","y_weryfikacyjne","t_weryfikacyjne");
+
